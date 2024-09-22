358a738bdf52a3233fcda2a1907a7e300e1621a7
LocatedMaple38 <144717449+LocatedMaple38@users.noreply.github.com>
2024-09-22 01:59:06 -0600

    Update main.s

--- main.s
+++ main.s
@@ -1,13 +1,41 @@
-.org E000
-
+PORTB0 = $8000
+PORTA0 = $8001
+DDRB0 = $8002
+DDRA0 = $8003
+PORTB1 = $8010
+PORTA1 = $8011
+DDRB1 = $8012
+DDRA1 = $8013
+PORTB2 = $8020
+PORTA2 = $801
+DDRB2 = $8022
+DDRA2 = $8023
+PORTB3 = $8030
+PORTA3 = $8031
+DDRB3 = $8032
+DDRA3 = $8033
+PORTB4 = $8040
+PORTA4 = $9041
+DDRB4 = $8042
+DDRA4 = $8043
+  .org $E000
+  
 reset:
-    lda #$ff
-    sta $6002
-    
-loop:
-    jmp loop
+  lda #%11111111
+  sta DDRB1
+  lda #%11100000
+  sta DDRA1
 
-.org fffa
-.word 0000
-.word reset
-.word 0000
+  lda #$03
+  sta PORTB1
+loop:
+  ror
+  sta PORTB1
+  jmp loop
+irq:
+  rti
+
+  .org $fffa
+  .word irq
+  .word reset
+  .word irq
